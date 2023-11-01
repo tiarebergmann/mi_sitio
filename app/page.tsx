30f21f3b5c25af3dc021d5884ef509062c5fa2de
tiare <tiarebrg@gmail.com>
2023-11-01 20:04:52 -0300

    se agrega numero random

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 
 export default function Home() {
+  const random = Math.random()*10;
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
      <h1>Mi sitio de prueba</h1>
      <h2>Hola</h2>
+     {random}
     </main>
   )
 }
